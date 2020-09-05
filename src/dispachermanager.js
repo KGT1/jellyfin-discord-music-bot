952e3e3599b0f3446ba01e91a70cdb130f2674ec
KGT1 <kilian.gamn@gmx.de>
2020-09-05 08:20:41 +0200

    add the discord audio dipacher manager

---
+++ src/dispachermanager.js
@@ -0,0 +1,13 @@
+var audioDispatcher;
+
+function setAudioDispatcher(par){
+    audioDispatcher=par;
+}
+function getAudioDispatcher(){
+    return audioDispatcher;
+}
+
+module.exports = {
+    setAudioDispatcher,
+    getAudioDispatcher
+}
