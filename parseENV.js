617b35d9afeb26ca3ac724ce8671a85fc72a7d64
KGT1 <kilian.gamn@gmx.de>
2020-09-08 14:02:00 +0200

    added docker functionality

---
+++ parseENV.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const filename='./config.json';
+const configfile = require(filename);
+
+if(!configfile["discord-prefix"])
+    configfile["discord-prefix"]=process.env.DISCORD_PREFIX;
+if(!configfile["token"])
+    onfigfile["token"]=process.env.DISCORD_TOKEN;
+if(!configfile["server-adress"])
+    configfile["server-adress"]=process.env.JELLYFIN_SERVER_ADDRESS;
+if(!configfile["jellyfin-username"])
+    configfile["jellyfin-username"]=process.env.JELLYFIN_USERNAME;
+if(!configfile["jellyfin-password"])
+    configfile["jellyfin-password"]=process.env.JELLYFIN_PASSWORD;
+if(!configfile["jellyfin-app-name"])
+    configfile["jellyfin-app-name"]=process.env.JELLYFIN_APP_NAME;
+
+
+fs.writeFile(filename, JSON.stringify(configfile,null,1), (err) => {
+    if (err) return console.log(err);
+});
