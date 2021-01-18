2e48f43b726ef43b3be92db417a186c17446e569
KGT1 <kilian.gamn@gmx.de>
2021-01-18 07:15:12 +0100

    add logging prefix and colors

--- src/index.js
+++ src/index.js
@@ -10,8 +10,33 @@ try {
 		handleChannelMessage
 	} = require("./messagehandler");
 	const log = require("loglevel");
+	const prefix = require('loglevel-plugin-prefix');
+	const chalk = require('chalk');
+	const colors = {
+		TRACE: chalk.magenta,
+		DEBUG: chalk.cyan,
+		INFO: chalk.blue,
+		WARN: chalk.yellow,
+		ERROR: chalk.red,
+	};
 
 	log.setLevel(CONFIG["log-level"]);
+
+
+	prefix.reg(log);
+	log.enableAll();
+
+	prefix.apply(log, {
+		format(level, name, timestamp) {
+			return `${chalk.gray(`[${timestamp}]`)} ${colors[level.toUpperCase()](level)} ${chalk.green(`${name}:`)}`;
+		},
+	});
+
+	prefix.apply(log.getLogger('critical'), {
+		format(level, name, timestamp) {
+			return chalk.red.bold(`[${timestamp}] ${level} ${name}:`);
+		},
+	});
 
 	jellyfinClientManager.init();
 	// TODO Error Checking as the apiclients is inefficent
